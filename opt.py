4ca2a9fc6efe028acf4203ae88d276b9d3695e06
Nathan Manske <nathan.manske@utexas.edu>
2017-05-17 22:50:58 -0500

    Grab only specified option definitions

--- opt.py
+++ opt.py
@@ -1,8 +1,18 @@
 import sys
 
-cmd = sys.argv[1]
+def main():
+    cmd = sys.argv[1]
+    opts = list(sys.argv[2].replace('-',''))
+    with open('definitions/' + cmd, 'r') as f:
+        definitions = f.read().split('\n\n')
+        for entry in definitions:
+            opts_line = entry.split('\n')[0]
+            for opt in opts:
+                if opt in opts_line:
+                    print(entry+'\n')
+                    opts.remove(opt)
+    for unknown_opt in opts:
+        print('Unknown option -' + unknown_opt)
 
-with open('definitions/' + cmd, 'r') as opt_list:
-    opt = opt_list.read()
-
-print(opt)
+if __name__ == '__main__':
+    main()
