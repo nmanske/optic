0e088f784e0a00b21f31ef9bc1a7ed89ddac5c14
Nathan Manske <nathan.manske@utexas.edu>
2017-05-18 23:43:04 -0500

    Add dictionaries to simplify options search

--- opt.py
+++ opt.py
@@ -2,30 +2,35 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('command', help='read options directory of specified command')
-parser.add_argument('options', help='decipher options ')
+parser.add_argument('command', help='parse options of specified command')
+parser.add_argument('options', help='learn more about these')
 parser.add_argument('-a', '--all', help='display all available options',
                     action='store_true')
 args = parser.parse_args()
 
-if args.command:
-    print('FOUND - args.command [' + args.command + ']')
-if args.options:
-    print('FOUND - args.options [' + args.options + ']')
-if args.all:
-    print('FOUND - args.all [' + args.all + ']')
-print()
+cmd_in = args.command
+opts_in = list(args.options.replace('-',''))
 
-cmd = sys.argv[1]
-opts = list(sys.argv[2].replace('-',''))
+opts_dict = {}
+defs_dict = {}
 
-with open('definitions/' + args.command, 'r') as f:
-    definitions = f.read().split('\n\n')
-    for entry in definitions:
-        opts_line = entry.split('\n')[0]
+# CONSTRUCT DICTIONARIES OF TARGET CMD
+with open('definitions/' + cmd_in, 'r') as f:
+    entries = f.read().split('\n\n')
+    for index, entry in enumerate(entries, start=1):
+        opts_anum = entry.replace(',','').replace('-','')
+        opts = opts_anum.split('\n')[0].split(' ')
         for opt in opts:
-            if opt in opts_line:
-                print(entry+'\n')
-                opts.remove(opt)
-for unknown_opt in opts:
-    print('Unknown option -' + unknown_opt)
+            opts_dict.update({opt:index})
+        defs = ' '.join(entry.split('\n')[1:])
+        defs_dict.update({index:defs})
+
+# FETCH OPTIONS IN USER GIVEN ORDER
+for opt in opts_in:
+    if opt in opts_dict:
+        dict_index = opts_dict.get(opt)
+        dict_entry = defs_dict.get(dict_index)
+        print('-' + opt)
+        print(dict_entry+'\n')
+    else:
+        print('-' + opt + '\nunknown option\n')
